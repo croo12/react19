2a01dc01732b56c47dcd666f43cf7c973c0e1fde
croo12 <its19447@gmail.com>
2024-07-24 23:41:37 +0900

    fix: 기본 컴포넌트 약간 변경 + vite 설정 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+/** @type {import('vate').UserConfig} */
 export default defineConfig({
   plugins: [react()],
 })
